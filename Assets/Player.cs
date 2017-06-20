777eb251bf49a09a6a5505b1bd874f1eee963ae2
Dustin Milotte <dmilotte@gmail.com>
2017-06-20 16:53:04 -0400

    Added navmesh for zombie, player spawnpoints

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public bool reSpawn = false;
+	public Transform [] spawnPoints;
+
+	private GameObject playerSpawnPoints;
+	private bool toggleSwitch= false;
+
+	// Use this for initialization
+	void Start () {
+		playerSpawnPoints = GameObject.Find("Player Spawn Points");
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (toggleSwitch != reSpawn) {
+			ReSpawn ();
+			reSpawn = false;
+		} 
+	}
+
+	void ReSpawn(){
+		transform.position = spawnPoints[Random.Range(1, spawnPoints.Length)].transform.position;
+	}
+}
