0459b22c74694afd74a357410245d3e80ca81894
Dustin Milotte <dmilotte@gmail.com>
2017-06-21 18:35:38 -0400

    added helicopter and callsounds

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+	private Camera eyes;
+	private float defaultFOV;
+	private bool zoomed = false;
+
+
+	// Use this for initialization
+	void Start () {
+		eyes = GetComponent<Camera>();
+		defaultFOV = eyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Zoom") && !zoomed){
+			eyes.fieldOfView = 1.5f*defaultFOV;
+			zoomed = true;
+		} else if (Input.GetButtonDown("Zoom")){
+			eyes.fieldOfView = defaultFOV;
+			zoomed = false;
+		}
+	}
+}
