1b08171fb1a7086a76702918d0cd0492038c538a
Dustin Milotte <dmilotte@gmail.com>
2017-07-05 19:27:19 -0400

    added day/night cycle

---
+++ Assets/Scripts/dayCycle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dayCycle : MonoBehaviour {
+	[Tooltip ("Set a value of minutes per second, try 60")]
+	public float timeScale;
+	
+	// Update is called once per frame
+	void Update () {
+		float degreesPerSecond = 360f / (60f * 60f * 24f); 
+		float angleThisFrame = Time.deltaTime * timeScale * degreesPerSecond; 
+		transform.Rotate(angleThisFrame, 0, 0);
+	}
+}
